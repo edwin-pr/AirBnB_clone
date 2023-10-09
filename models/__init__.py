a5eed6c7f73b4378afdd0c0032cebb23aa4670bb
root <root@DESKTOP-DMRT987>
2023-10-09 21:58:37 +0300

    deletion of models dir

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from models.engine.file_storage import FileStorage
-
-# Create a unique FileStorage instance
-storage = FileStorage()
-
-# Call the reload() method to deserialize existing data (if the JSON file exists)
-storage.reload()
